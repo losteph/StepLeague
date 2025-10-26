5ef331666aaccd1bfb57090ed9b8348f126c47ad
Stefano Di Lena <stephdln02@gmail.com>
2025-10-26 10:29:59 +0100

    Update classifica.js

--- js/classifica.js
+++ js/classifica.js
@@ -1,44 +1,18 @@
 import { squadre, partite } from './data.js';
+import { calcolaClassifica } from './utils.js';
 
-const classifica = squadre.map(s => ({
-  nome: s.nome,
-  giocate: 0,
-  vinte: 0,
-  pareggiate: 0,
-  perse: 0,
-  golFatti: 0,
-  golSubiti: 0,
-  punti: 0
-}));
+const tbody = document.querySelector('#table-classifica tbody');
+function render(){
+  const cls = calcolaClassifica(squadre, partite);
+  tbody.innerHTML = '';
+  cls.forEach((r, i) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${i+1}</td><td>${r.nome}</td><td>${r.giocate}</td><td>${r.vinte}</td><td>${r.pareggiate}</td><td>${r.perse}</td><td>${r.gf}</td><td>${r.gs}</td><td>${r.dg}</td><td>${r.punti}</td>`;
+    tbody.appendChild(tr);
+  });
+}
 
-partite.forEach(p => {
-  const casa = classifica.find(t => t.nome === p.casa);
-  const trasf = classifica.find(t => t.nome === p.trasferta);
+render();
 
-  casa.giocate++;
-  trasf.giocate++;
-  casa.golFatti += p.golCasa;
-  casa.golSubiti += p.golTrasferta;
-  trasf.golFatti += p.golTrasferta;
-  trasf.golSubiti += p.golCasa;
-
-  if (p.golCasa > p.golTrasferta) {
-    casa.vinte++; trasf.perse++; casa.punti += 3;
-  } else if (p.golCasa < p.golTrasferta) {
-    trasf.vinte++; casa.perse++; trasf.punti += 3;
-  } else {
-    casa.pareggiate++; trasf.pareggiate++;
-    casa.punti++; trasf.punti++;
-  }
-});
-
-classifica.sort((a, b) => b.punti - a.punti || (b.golFatti - b.golSubiti) - (a.golFatti - a.golSubiti));
-
-// Mostra tabella
-const tbody = document.querySelector("#classifica tbody");
-classifica.forEach(t => {
-  const tr = document.createElement("tr");
-  tr.innerHTML = `<td>${t.nome}</td><td>${t.punti}</td><td>${t.golFatti}</td><td>${t.golSubiti}</td>`;
-  tbody.appendChild(tr);
-});
-
+// espone funzione per altre pagine
+window.__torneo_render_classifica = render;
