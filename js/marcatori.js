c02ae6d38322cf9ec638ad3d3948569c66f4504c
Stefano Di Lena <stephdln02@gmail.com>
2025-10-26 10:11:33 +0100

    Add player sorting and display in marcatori.js

--- js/marcatori.js
+++ js/marcatori.js
@@ -1,1 +1,12 @@
+import { squadre } from './data.js';
+
+const giocatori = squadre.flatMap(s => s.giocatori.map(g => ({ squadra: s.nome, ...g })));
+giocatori.sort((a, b) => b.gol - a.gol);
+
+const tbody = document.querySelector("#marcatori tbody");
+giocatori.forEach(g => {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `<td>${g.nome}</td><td>${g.squadra}</td><td>${g.gol}</td><td>${g.assist}</td>`;
+  tbody.appendChild(tr);
+});
 
