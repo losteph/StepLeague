fab3674399b010d5c27103738b0696029717bae9
Stefano Di Lena <stephdln02@gmail.com>
2025-10-26 10:54:08 +0100

    Fix syntax error in forEach loop

--- js/marcatori.js
+++ js/marcatori.js
@@ -2,10 +2,11 @@ import { squadre } from './data.js';
 import { calcolaMarcatori } from './utils.js';
 
 const tbody = document.querySelector('#table-marcatori tbody');
+
 function render(){
   const lista = calcolaMarcatori(squadre);
   tbody.innerHTML = '';
-  lista.forEach((g, i)=>{
+  lista.forEach((g,i)=>{
     const tr = document.createElement('tr');
     tr.innerHTML = `<td>${i+1}</td><td>${g.nome}</td><td>${g.squadra}</td><td>${g.gol}</td><td>${g.assist}</td><td>${g.gialli}</td><td>${g.rossi}</td>`;
     tbody.appendChild(tr);
